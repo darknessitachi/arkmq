5a6f88ce6ee97f150051a71dabf9daee35d1eced
darkness <darkness@darknessdembp>
2017-01-11 17:11:28 +0800

    dd

--- src/main/java/com/abigdreamer/arkmq/ServerUI.java
+++ src/main/java/com/abigdreamer/arkmq/ServerUI.java
@@ -9,15 +9,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
 import java.net.BindException;
 import java.net.ServerSocket;
-import java.net.Socket;
 import java.util.ArrayList;
-import java.util.StringTokenizer;
 
 import javax.swing.DefaultListModel;
 import javax.swing.JButton;
@@ -31,6 +26,9 @@ import javax.swing.JSplitPane;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 import javax.swing.border.TitledBorder;
+
+import com.abigdreamer.arkmq.bio.ClientThread;
+import com.abigdreamer.arkmq.bio.ServerThread;
 
 public class ServerUI {
 
@@ -55,8 +53,6 @@ public class ServerUI {
 	private ArrayList<ClientThread> clients;
 
 	private boolean isStart = false;
-
-	
 
 	// 执行消息发送
 	public void send() {
@@ -279,166 +275,6 @@ public class ServerUI {
 			clients.get(i).getWriter().flush();
 		}
 	}
-
-	// 服务器线程
-	class ServerThread extends Thread {
-		private ServerSocket serverSocket;
-		private int max;// 人数上限
-
-		// 服务器线程的构造方法
-		public ServerThread(ServerSocket serverSocket, int max) {
-			this.serverSocket = serverSocket;
-			this.max = max;
-		}
-
-		public void run() {
-			while (true) {// 不停的等待客户端的链接
-				try {
-					Socket socket = serverSocket.accept();
-					if (clients.size() == max) {// 如果已达人数上限
-						BufferedReader r = new BufferedReader(
-								new InputStreamReader(socket.getInputStream()));
-						PrintWriter w = new PrintWriter(socket
-								.getOutputStream());
-						// 接收客户端的基本用户信息
-						String inf = r.readLine();
-						StringTokenizer st = new StringTokenizer(inf, "@");
-						User user = new User(st.nextToken(), st.nextToken());
-						// 反馈连接成功信息
-						w.println("MAX@服务器：对不起，" + user.getName()
-								+ user.getIp() + "，服务器在线人数已达上限，请稍后尝试连接！");
-						w.flush();
-						// 释放资源
-						r.close();
-						w.close();
-						socket.close();
-						continue;
-					}
-					ClientThread client = new ClientThread(socket);
-					client.start();// 开启对此客户端服务的线程
-					clients.add(client);
-					listModel.addElement(client.getUser().getName());// 更新在线列表
-					contentArea.append(client.getUser().getName()
-							+ client.getUser().getIp() + "上线!\r\n");
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-	}
-
-	// 为一个客户端服务的线程
-	class ClientThread extends Thread {
-		private Socket socket;
-		private BufferedReader reader;
-		private PrintWriter writer;
-		private User user;
-
-		public BufferedReader getReader() {
-			return reader;
-		}
-
-		public PrintWriter getWriter() {
-			return writer;
-		}
-
-		public User getUser() {
-			return user;
-		}
-
-		// 客户端线程的构造方法
-		public ClientThread(Socket socket) {
-			try {
-				this.socket = socket;
-				reader = new BufferedReader(new InputStreamReader(socket
-						.getInputStream()));
-				writer = new PrintWriter(socket.getOutputStream());
-				// 接收客户端的基本用户信息
-				String inf = reader.readLine();
-				StringTokenizer st = new StringTokenizer(inf, "@");
-				user = new User(st.nextToken(), st.nextToken());
-				// 反馈连接成功信息
-				writer.println(user.getName() + user.getIp() + "与服务器连接成功!");
-				writer.flush();
-				// 反馈当前在线用户信息
-				if (clients.size() > 0) {
-					String temp = "";
-					for (int i = clients.size() - 1; i >= 0; i--) {
-						temp += (clients.get(i).getUser().getName() + "/" + clients
-								.get(i).getUser().getIp())
-								+ "@";
-					}
-					writer.println("USERLIST@" + clients.size() + "@" + temp);
-					writer.flush();
-				}
-				// 向所有在线用户发送该用户上线命令
-				for (int i = clients.size() - 1; i >= 0; i--) {
-					clients.get(i).getWriter().println(
-							"ADD@" + user.getName() + user.getIp());
-					clients.get(i).getWriter().flush();
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-
-		@SuppressWarnings("deprecation")
-		public void run() {// 不断接收客户端的消息，进行处理。
-			String message = null;
-			while (true) {
-				try {
-					message = reader.readLine();// 接收客户端消息
-					if (message.equals("CLOSE"))// 下线命令
-					{
-						contentArea.append(this.getUser().getName()
-								+ this.getUser().getIp() + "下线!\r\n");
-						// 断开连接释放资源
-						reader.close();
-						writer.close();
-						socket.close();
-
-						// 向所有在线用户发送该用户的下线命令
-						for (int i = clients.size() - 1; i >= 0; i--) {
-							clients.get(i).getWriter().println(
-									"DELETE@" + user.getName());
-							clients.get(i).getWriter().flush();
-						}
-
-						listModel.removeElement(user.getName());// 更新在线列表
-
-						// 删除此条客户端服务线程
-						for (int i = clients.size() - 1; i >= 0; i--) {
-							if (clients.get(i).getUser() == user) {
-								ClientThread temp = clients.get(i);
-								clients.remove(i);// 删除此用户的服务线程
-								temp.stop();// 停止这条服务线程
-								return;
-							}
-						}
-					} else {
-						dispatcherMessage(message);// 转发消息
-					}
-				} catch (IOException e) {
-					e.printStackTrace();
-				}
-			}
-		}
-
-		// 转发消息
-		public void dispatcherMessage(String message) {
-			StringTokenizer stringTokenizer = new StringTokenizer(message, "@");
-			String source = stringTokenizer.nextToken();
-			String owner = stringTokenizer.nextToken();
-			String content = stringTokenizer.nextToken();
-			message = source + "说：" + content;
-			contentArea.append(message + "\r\n");
-			if (owner.equals("ALL")) {// 群发
-				for (int i = clients.size() - 1; i >= 0; i--) {
-					clients.get(i).getWriter().println(message + "(多人发送)");
-					clients.get(i).getWriter().flush();
-				}
-			}
-		}
-	}
+	
 }
 
